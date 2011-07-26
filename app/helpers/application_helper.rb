315f53da397705e749f1d8759c3e9ce063caef0e
lackita <lackita+github@gmail.com>
2011-07-26 08:48:35 -0400

    Chapter 5 exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+  # Return the image tag html for the logo
+  def logo
+    return image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
